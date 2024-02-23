f06ec9e2ded821518e8e1e3d9c6b3f57662b92b3
Saurav <saurav.satpute5@gmail.com>
2024-02-23 18:31:11 +0530

    2nd Commit

--- src/components/ChoosePlayer.js
+++ src/components/ChoosePlayer.js
@@ -1,16 +1,19 @@
 import React from 'react'
-
+import style from './ChoosePlayer.module.css'
 const ChoosePlayer = () => {
   return (
-    <div>
-        X O
-        <div>
+    <div className={style.flexColumn} style={{paddingTop:'150px'}}>
+        <div className={style.playerHeading}>
+                <span style={{color:'#31C4BE', fontWeight:800}}>X</span>
+                <span style={{color:'#F2B237', fontWeight:800}}>O</span>
+        </div>
+        <div style={{backgroundColor:'#1F3540', color:'#fff', textAlign:'center', padding:'1.5rem'}}>
             Pick Player
-            <div className='PickPlayer'>
-                <div>
+            <div className={style.flexRow} style={{ backgroundColor:'#192A32', padding:'5px', margin:'5px'}}>
+                <div className={style.OptBtn} style={{color:'#D9D9D9', backgroundColor:'#192A32'}}>
                     X
                 </div>
-                <div>
+                <div className={style.OptBtn} style={{color:'#192A32', backgroundColor:'#D9D9D9'}}>
                     O
                 </div>
             </div>
--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import style from './Phone.module.css'
+import ChoosePlayer from './ChoosePlayer'
 
 const phone = () => {
   return (
     <div className={style.phone}>
-        
+        <ChoosePlayer/>
     </div>
   )
 }
